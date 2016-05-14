a3161f605d5058cb3eb02ada96faaa428da79341
Lyle <denmanlp@gmail.com>
2016-05-14 15:18:05 -0500

    init commit

---
+++ mirrorDraw.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Lyle Denman on 5/14/16.
+ */
+
+(function(){
+  var debug = true;
+
+  function log(arg) {
+    if (debug)
+      console.log(arg);
+  }
+
+  var canvas = document.getElementById('canvas');
+  log(canvas.height);
+
+
+})();
