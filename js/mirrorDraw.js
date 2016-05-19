5484e810954f7596e2f0ca1e468ef2a8d200ed92
Lyle <denmanlp@gmail.com>
2016-05-19 00:51:19 -0500

    Change classes to ES6 classes; pass 'global' data into classes

--- js/mirrorDraw.js
+++ js/mirrorDraw.js
@@ -2,110 +2,119 @@
  * Created by Lyle Denman on 5/14/16.
  * With lots of help from: www.simonsarris.com/blog/510-making-html5-canvas-useful
  */
-
 (function(){
   /************************/
   /***** Square class *****/
   /************************/
-  function Square(x, y, w, h, fillColor) {
-    this.x = x || 0; // default to 0 if no x provided
-    this.y = y || 0;
-    this.w = w || 1;
-    this.h = h || 1;
-    this.fillColor = fillColor || "#FFFFFF";
-  }
+  class Square{
+    constructor(x, y, w, fillColor) {
+      this.x = x || 0; // Default to 0 if no x provided, etc
+      this.y = y || 0;
+      this.width = w || 1;
+      this.height = w || 1;
+      this.fillColor = fillColor || "#FFFFFF";
+    }
 
-  Square.prototype.draw = function(ctx) {
-    ctx.fillStyle = this.fillColor;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-  };
+    draw(ctx) { // Draw square to canvas
+      ctx.fillStyle = this.fillColor;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
   /************************/
   /*** CanvasState class **/
   /************************/
-  function CanvasState(canvas) {
-    var myState = this;
+  class CanvasState {
+    constructor(canvas, numRows, squareDim) {
+      this.canvas = canvas;
+      this.numRows = numRows;
+      this.squareDim = squareDim;
+      this.shapes = []; // Store the shapes drawn to the canvas
+      this.listenMousedown();
+    }
 
-    // Track object drawn to the canvas
-    myState.shapes = [];
+    listenMousedown() {
+      this.canvas.addEventListener('mousedown', (e) => {
+        var mouse = CanvasState.getMouse(e);
+        var mx = mouse.x;
+        var my = mouse.y;
 
-    // Detect events
-    canvas.addEventListener('mousedown', function(e) {
-      var mouse = myState.getMouse(e);
-      var mx = mouse.x;
-      var my = mouse.y;
+        var shapeIndex = this.getShapeIndexAt(mx, my);
+        if (shapeIndex != -1) {
+          var mirrorIndex = CanvasState.getMirror(shapeIndex, this.numRows);
 
-      var shape = myState.getShapeIndexAt(mx, my);
-      if (shape != -1) {
-        var mirrorShape = myState.getMirror(shape, sqRoot);
-        if (mirrorShape != shape) {
-          // if board is odd-numbered, a shape in the center column has itself as a mirror
-          myState.flipColor(myState.shapes[mirrorShape]);
-        }
-        myState.flipColor(myState.shapes[shape]);
-        ctx.clearRect(0, 0, canvasDim, canvasDim); // clear board
-        drawSquares(); // draw board
-      }
-    });
-  }
-
-  CanvasState.prototype.getShapeIndexAt = function(x, y) {
-    for (var i = 0; i < myState.shapes.length; i++) {
-      if (myState.shapes[i] !== undefined) {
-        if (x >= myState.shapes[i].x && x < myState.shapes[i].x + gridSize) {
-          if (y >= myState.shapes[i].y && y < myState.shapes[i].y + gridSize) {
-            return i;
+          if (mirrorIndex != shapeIndex) {
+            // If numRows is odd, a shape in the center column has itself as a mirror
+            // So no need to flipColor() twice
+            CanvasState.flipColor(this.shapes[mirrorIndex]);
           }
+          CanvasState.flipColor(this.shapes[shapeIndex]);
+          ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
+          this.drawSquares(); // draw board
+        }
+      });
+    }
+
+    createSquares () {
+      var count = 0;
+      for (var x = 0; x < this.canvas.width; x+=this.squareDim) {
+        for (var y = 0; y < this.canvas.height; y+=this.squareDim) {
+          this.shapes[count] = new Square(x, y, this.squareDim);
+          count++;
         }
       }
     }
-    return -1; // Error: no shape found
-  };
 
-  CanvasState.prototype.getMouse = function(e) {
+    drawSquares() {
+      for (var i = 0; i < this.shapes.length; i++) {
+        if (this.shapes[i] !== undefined) {
+          this.shapes[i].draw(ctx);
+        }
+      }
+    }
+
+    getShapeIndexAt(x, y) {
+      for (var i = 0; i < this.shapes.length; i++) {
+        if (this.shapes[i] !== undefined) {
+          if (x >= this.shapes[i].x && x < this.shapes[i].x + this.squareDim) {
+            if (y >= this.shapes[i].y && y < this.shapes[i].y + this.squareDim) {
+              return i;
+            }
+          }
+        }
+      }
+      return -1; // Error: no shape found
+    }
+
+  }
+
+  /************************/
+  /***** CanvasState ******/
+  /*** Static Functions ***/
+  /************************/  
+  CanvasState.getMouse = function(e) {
     return {x: e.offsetX, y: e.offsetY};
   };
 
-  CanvasState.prototype.flipColor = function(shape) {
+  CanvasState.flipColor = function(shape) {
     if (shape.fillColor == "#000000") {
-      shape.fillColor = "#ffffff";
+      shape.fillColor = "#FFFFFF";
     } else {
       shape.fillColor = "#000000";
     }
   };
 
-  CanvasState.prototype.getMirror = function(index, n) {
+  CanvasState.getMirror = function(index, numRows) {
     // Return a given square's mirrored square (across the y-axis)
     if (index === 0) {
-      return Math.floor(n * (n-1));
+      return Math.floor(numRows * (numRows-1));
     }
 
-    var nOrZero = (index % n === 0) ? n : 0;
-    return n*n - (n * (Math.ceil(index / n))) + (index % n) - nOrZero;
+    var nOrZero = (index % numRows === 0) ? numRows : 0;
+    return numRows*numRows - (numRows * (Math.ceil(index / numRows))) +
+        (index % numRows) - nOrZero;
   };
-
-  CanvasState.prototype.createSquares = function() {
-    var count = 0;
-    for (var i = 0; i < canvasDim ; i+=gridSize) {
-      for (var j = 0; j < canvasDim; j+=gridSize) {
-        ctx.fillStyle = "#ffffff";
-        myState.shapes[count] = new Square(i, j, gridSize, gridSize, ctx.fillStyle);
-        count++;
-      }
-    }
-  };
-
-  /************************/
-  /*****   Helpers    *****/
-  /************************/
-  function drawSquares() {
-    for (var i = 0; i < myState.shapes.length; i++) {
-      if (myState.shapes[i] !== undefined) {
-        myState.shapes[i].draw(ctx);
-      }
-    }
-  }
-
+  
   /************************/
   /****   MAIN EVENT   ****/
   /************************/
@@ -113,17 +122,18 @@
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
   }
-  var sqRoot = 16;
+  var numRows = 16;
   var canvasDim = canvas.width;
 
-  // ensure dimensions align for pixel-perfection
-  while (canvasDim % sqRoot !== 0) {
+  // Update canvas dimensions for pixel-perfection
+  while (canvasDim % numRows !== 0) {
     canvasDim++;
   }
   canvas.width = canvas.height = canvasDim;
 
-  var gridSize = canvas.width/sqRoot;
-  var myState = new CanvasState(canvas);
-  myState.createSquares();
-  drawSquares();
+  var squareDim = canvasDim/numRows; // Set dimensions of the square
+
+  var drawingBoard = new CanvasState(canvas, numRows, squareDim);
+  drawingBoard.createSquares();
+  drawingBoard.drawSquares();
 })();
